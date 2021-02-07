8e95c9e7e7eb6edb5a2da41435358c70f1ad53bd
Daffa Aditya Rahman <41433315+DaffaAdityaCS@users.noreply.github.com>
2021-02-07 21:42:56 +0700

    Add files via upload

---
+++ day15_fbDonwloader_py/donwloader.py
@@ -0,0 +1,29 @@
+import re
+import requests
+import urllib.request
+
+
+def donwload():
+    #take a link from user
+    link = input(str("insert URL:"))
+
+    #read html data 
+    html = requests.get(link)
+
+    #parse URL
+    try:
+        url = re.search('hd_src:"(.+?)"', html.text) [1]
+        print("HD Video")
+    except:
+        url = re.search('sd_src:"(.+?)"' ,html.text) [1]    
+        print("SD Video")
+
+    inpt = input(str("video Name: "))
+    name = inpt + str(".mp4")
+    
+    print("Donwloading..")
+    urllib.request.urlretrieve(url, name)
+    print("Donwload Done")
+#infinite loop donwload
+
+donwload()
